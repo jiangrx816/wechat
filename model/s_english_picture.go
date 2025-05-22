94b704a5fbc8f6b70b7b8ef4ce40efadf0de3070
ruoxi <ruoxi@163.com>
2025-05-22 17:04:57 +0800

    修改字段

--- model/s_english_picture.go
+++ model/s_english_picture.go
@@ -7,7 +7,7 @@ import (
 // 英语绘本
 type SEnglishPicture struct {
 	Id         uint      `gorm:"column:id;type:int(11) unsigned;primary_key;AUTO_INCREMENT;comment:主键id" json:"id"`
-	BookId     string    `gorm:"column:book_id;type:varchar(255);comment:绘本id;NOT NULL" json:"book_id"`
+	BookId     string    `gorm:"column:book_id;type:char(32);comment:绘本id;NOT NULL" json:"book_id"`
 	Title      string    `gorm:"column:title;type:varchar(1024);comment:标题;NOT NULL" json:"title"`
 	Icon       string    `gorm:"column:icon;type:varchar(1024);comment:封面图;NOT NULL" json:"icon"`
 	Type       int       `gorm:"column:type;type:tinyint(1);default:1;comment:级别;NOT NULL" json:"type"`
--- service/wechat_service/service_handle.go
+++ service/wechat_service/service_handle.go
@@ -1,20 +1,17 @@
 package wechat_service
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
+	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jiangrx816/wechat/common/request"
-	"github.com/jiangrx816/wechat/common/response"
 	"github.com/jiangrx816/wechat/core/server/api"
 	"github.com/jiangrx816/wechat/model"
+	"github.com/jiangrx816/wechat/utils"
 )
 
 /**
@@ -32,110 +29,59 @@ func (ps *WechatService) ApiServiceEnglishHandleData(ctx *gin.Context, level int
 }
 
 func (ps *WechatService) HandleEnglishDataInfo(ctx *gin.Context, level int, filePath string) (err error) {
-	var bookInitList []model.SEnglishPicture
-	db := model.Default().Model(&model.SEnglishPicture{}).Debug()
-	db = db.Where("id >0")
-	db = db.Order("id asc")
+	var bookInitList []model.SEnglishPictureInfo
+	db := model.Default().Model(&model.SEnglishPictureInfo{}).Debug()
+	db = db.Where("id >0").Order("id asc")
 	db.Find(&bookInitList)
 
-	var bookIdList []string
-	for _, item := range bookInitList {
-		bookIdList = append(bookIdList, item.BookId)
+	for _, book := range bookInitList {
+		dirPath := "/Users/jiang/小程序/file/" + book.BookId
+		savePath := dirPath + "/" + strconv.Itoa(int(book.Id)) + "_b.jpg"
+		flag, _ := utils.FileExists(savePath)
+		if condition := !flag; condition {
+			fmt.Printf("文件不存在: %s\n", savePath)
+			ps.downloadAndSave(book)
+		}
 	}
 
-	for _, bookId := range bookIdList {
-		bookIdInt, _ := strconv.Atoi(bookId)
-		ps.ApiServiceEnglishBookSearch(bookIdInt)
-	}
-	return
+	return nil
 }
 
-func (ps *WechatService) ApiServiceEnglishBookSearch(bookId int) {
-	// 创建要发送的数据
-	data := request.RequestEnglishInfoParam{
-		HDt:    3,
-		HDid:   "16851689799320000",
-		Did:    "16851689799320000",
-		HCH:    "miniprogram",
-		HTs:    "1685168991352",
-		HM:     0,
-		Zone:   28800,
-		Token:  "",
-		HLc:    "zh",
-		Cate:   1,
-		Atype:  3,
-		BookId: bookId,
-		Limit:  200,
-	}
+// 下载和保存 MP3 文件
+func (ps *WechatService) downloadAndSave(book model.SEnglishPictureInfo) error {
+	fileUrl := book.BPic
+	dirPath := "/Users/jiang/小程序/file/" + book.BookId
+	utils.ExistDir(dirPath)
+	savePath := dirPath + "/" + strconv.Itoa(int(book.Id)) + "_b.jpg"
 
-	// 将数据编码为 JSON 格式
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		log.Fatalf("Error encoding JSON: %v", err)
-	}
-
-	// 创建 POST 请求
-	url := "https://www.ipalfish.com/klian/ugc/picturebook/official/product/bookid/page/list"
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-	if err != nil {
-		log.Fatalf("Error creating request: %v", err)
-	}
-
-	// 设置请求头
-	req.Header.Set("Content-Type", "application/json")
-
-	// 发送请求
+	// 创建 HTTP 客户端
 	client := &http.Client{}
-	respH, err := client.Do(req)
+	resp, err := client.Get(fileUrl)
 	if err != nil {
-		log.Fatalf("Error sending request: %v", err)
+		return fmt.Errorf("下载请求失败: %w", err)
 	}
-	defer respH.Body.Close()
+	defer resp.Body.Close()
 
-	// 读取响应
-	body, err := ioutil.ReadAll(respH.Body)
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("服务器返回错误状态码: %d", resp.StatusCode)
+	}
+
+	// 确保目录存在
+	if err := os.MkdirAll(filepath.Dir(savePath), 0755); err != nil {
+		return fmt.Errorf("创建目录失败: %w", err)
+	}
+
+	// 创建文件
+	outFile, err := os.Create(savePath)
 	if err != nil {
-		log.Fatalf("Error reading response: %v", err)
+		return fmt.Errorf("文件创建失败: %w", err)
 	}
-	jsonFilePath := fmt.Sprintf("/Users/jiang/小程序/info/%d.json", bookId)
+	defer outFile.Close()
 
-	// 将 拓扑数据类型数据的 JSON 内容写入文件
-	if err = os.WriteFile(jsonFilePath, []byte(body), 0644); err != nil {
-		log.Fatalf("数据写入文件失败: %v", err)
-	}
-	fmt.Printf("数据写入文件成功: %v \n", jsonFilePath)
-}
-
-func (ps *WechatService) HandleEnglishData(ctx *gin.Context, level int, filePath string) (err error) {
-	// 1. 读取 JSON 文件
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		panic(fmt.Sprintf("读取文件失败: %v", err))
+	// 写入内容
+	if _, err := io.Copy(outFile, resp.Body); err != nil {
+		return fmt.Errorf("文件写入失败: %w", err)
 	}
 
-	// 2. 解析 JSON 到结构体
-	var handleData response.BYJSONData
-	err = json.Unmarshal(data, &handleData)
-	if err != nil {
-		panic(fmt.Sprintf("解析 JSON 失败: %v", err))
-	}
-
-	// 3. 处理数据
-	var englishPictureTempList []model.SEnglishPicture
-	itemList := handleData.Data.Ent.Items
-	for _, item := range itemList {
-		// 处理每一项数据
-		englishPictureTempList = append(englishPictureTempList, model.SEnglishPicture{
-			BookId: strconv.FormatInt(item.Bookid, 10),
-			Title:  item.Title,
-			Icon:   item.Cover.Tiny,
-			Type:   level,
-			Status: 1,
-		})
-	}
-	err = model.Default().Model(&model.SEnglishPicture{}).Debug().Create(&englishPictureTempList).Error
-	if condition := err != nil; condition {
-		panic(fmt.Sprintf("入库 失败: %v", err))
-	}
-	return
+	return nil
 }
